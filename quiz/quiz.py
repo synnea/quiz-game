2d3a9091d60a9242426e8b365643543c9849c53c
Carina <carrie.poell@gmail.com>
2019-06-27 18:49:17 +0200

    half of the second video

---
+++ quiz/quiz.py
@@ -0,0 +1,54 @@
+def show_menu():
+    print("1. Ask Questions")
+    print("2. Add a question")
+    print("3. Exit game")
+
+    option = input("Enter option: ")
+    return option
+
+def ask_question():
+    questions = []
+    answers = []
+
+    with open("questions.txt", "r") as file:
+        lines = file.read().splitlines()
+
+    for i, text in enumerate(lines):
+        if i%2 == 0:
+            questions.append(text)
+        
+        else:
+            answers.append(text)
+
+    for question, answer in zip(questions, answers):
+        guess = input(question + ">")
+
+def add_question():
+    print("")
+    question = input("Enter a question\n> ")
+
+    print("")
+    print("Ok then, tell me the answer")
+    answer = input("{0}\n> ".format(question))
+
+    file = open("questions.txt", "a")
+    file.write(question + "\n")
+    file.write(answer + "\n")
+    file.close()
+
+1
+
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            ask_question()
+        elif option == "2":
+            add_question()
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+game_loop()
---
+++ write.py
@@ -0,0 +1,6 @@
+f = open('newfile.txt', 'a')
+lines = ['Hello', 'World', 'Welcome', 'To', 'File IO']
+text = '\n'.join(lines)
+f.writelines(text)
+f.close()
+
